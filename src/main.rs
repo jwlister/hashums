7dc1c18775742e977cd331fbf9c20b11d76a0f57
Jeremiah Lister <jwlister@protonmail.com>
2021-10-24 08:38:51 -0600

    Use more functional style

--- src/main.rs
+++ src/main.rs
@@ -8,57 +8,50 @@ use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 use walkdir::WalkDir;
 
 fn main() {
-    let args: Vec<_> = env::args().skip(1).collect();
-
-    if args.is_empty() {
-        return;
-    }
-
-    let mut paths = Vec::new();
-    let mut hashes = Vec::new();
-
     //Only read 100 MiB at a time, enabling us to hash arbitrarily large files without
     //using much memory.
     const BUF_LEN: usize = 104_857_600;
     let mut buf = Vec::with_capacity(BUF_LEN);
-    for path in args.iter().map(|arg| Path::new(arg)).filter(|p| p.exists()) {
-        let mut push_hash_and_path = |path| {
-            hashes.push(hash_in_chunks(
-                &mut File::open(&path).unwrap(),
-                &mut buf,
-                BUF_LEN,
-            ));
-            paths.push(path);
-        };
 
-        if path.is_dir() {
-            WalkDir::new(path)
+    let path_iter = || {
+        env::args()
+            .skip(1)
+            .map(|arg| Path::new(&arg).to_owned())
+            .filter(|p| p.exists())
+    };
+
+    let paths_and_hashes: Vec<_> = path_iter()
+        .filter(|p| p.is_dir())
+        .flat_map(|p| {
+            WalkDir::new(p)
                 .into_iter()
                 .map(|e| e.unwrap())
                 .filter(|e| e.path().is_file())
-                .for_each(|e| push_hash_and_path(e.path().to_owned()));
-        } else if path.is_file() {
-            push_hash_and_path(path.to_owned());
-        }
-    }
+                .map(|e| e.path().to_owned())
+        })
+        .chain(path_iter().filter(|p| p.is_file()))
+        .map(|p| {
+            let mut file = File::open(&p).unwrap();
+            (p, hash_in_chunks(&mut file, &mut buf, BUF_LEN))
+        })
+        .collect();
 
     println!("SHA-256\n");
 
-    if paths.len() > 1 {
+    if paths_and_hashes.len() > 1 {
         //When creating the combined hash, sort lexicographically by path, that way it doesn't
         //matter what order the files happened to get sent in; the same set of files will always
         //produce the same combined hash.
 
-        let mut combined_hasher = Sha256::new();
+        let mut hasher = Sha256::new();
 
-        paths
+        paths_and_hashes
             .iter()
-            .zip(hashes.iter())
-            .sorted_unstable_by_key(|(path, _)| *path)
-            .for_each(|(_, hash)| combined_hasher.update(hash));
+            .sorted_unstable_by_key(|(path, _)| path)
+            .for_each(|(_, hash)| hasher.update(hash));
 
         println!("Combined");
-        println!("{}\n", format!("{:X}", combined_hasher.finalize()));
+        println!("{}\n", format!("{:X}", hasher.finalize()));
 
         //Eye-catching green/red for match/no match.
         let mut stdout = StandardStream::stdout(ColorChoice::Always);
@@ -70,7 +63,7 @@ fn main() {
             writeln!(&mut stdout, "{}", s).unwrap();
         };
 
-        if hashes.iter().all_equal() {
+        if paths_and_hashes.iter().map(|(_, hash)| hash).all_equal() {
             print_with_color("SAME\n", Color::Green);
         } else {
             print_with_color("DIFFERENT\n", Color::Red);
@@ -79,7 +72,7 @@ fn main() {
         stdout.reset().unwrap();
     }
 
-    for (path, hash) in paths.iter().zip(hashes.iter()) {
+    for (path, hash) in &paths_and_hashes {
         println!("{}\n{}\n", path.display(), hash);
     }
 
